3bd192fe5d4f9d6f02eb90dae31690f93265fee8
jeffhaskin <jeffhaskin1@gmail.com>
2026-03-28 01:07:17 +0100

    br-plan-present-nj9 bootstrap empty app

---
+++ src/client/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main>
+      <h1>plan-present</h1>
+      <p>Client shell ready.</p>
+    </main>
+  );
+}
---
+++ src/server/index.ts
@@ -0,0 +1,1 @@
+console.log("plan-present server placeholder");
